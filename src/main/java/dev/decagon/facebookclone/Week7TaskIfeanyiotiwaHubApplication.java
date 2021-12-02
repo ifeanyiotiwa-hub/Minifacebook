44f77e9df33ee044c38f1679719e382b6031721c
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2021-12-02 08:55:22 +0100

    Project setup

---
+++ src/main/java/dev/decagon/facebookclone/Week7TaskIfeanyiotiwaHubApplication.java
@@ -0,0 +1,13 @@
+package dev.decagon.facebookclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7TaskIfeanyiotiwaHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7TaskIfeanyiotiwaHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/decagon/facebookclone/Week7TaskIfeanyiotiwaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.decagon.facebookclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7TaskIfeanyiotiwaHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
